4dad9e98d48c7fc95e3859611ef5ba9244b51fa4
sunyiqiu <1607109698@qq.com>
2024-11-22 11:11:38 +0800

    build: add error request return status is 200

--- src/main/java/org/interaction/interactionbackend/exception/GlobalExceptionHandler.java
+++ src/main/java/org/interaction/interactionbackend/exception/GlobalExceptionHandler.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.interaction.interactionbackend.util.ResponseBuilder;
 import org.interaction.interactionbackend.vo.ResponseVO;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
@@ -14,6 +16,7 @@ public class GlobalExceptionHandler {
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(value = WorldViewException.class)
+    @ResponseStatus(HttpStatus.OK)
     public ResponseVO handleAIExternalException(WorldViewException e) {
 //        e.printStackTrace();
         logger.debug("AI External Exception: {}", e.getMessage());
