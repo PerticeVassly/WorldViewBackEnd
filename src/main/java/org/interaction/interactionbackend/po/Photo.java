1f736535d3cba54b5c3ae09aaafe51c677be9985
sunyiqiu <1607109698@qq.com>
2024-12-03 20:31:45 +0800

    fix entity bug

--- src/main/java/org/interaction/interactionbackend/po/Photo.java
+++ src/main/java/org/interaction/interactionbackend/po/Photo.java
@@ -4,15 +4,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.interaction.interactionbackend.enums.PhotoTheme;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
 public class Photo {
 
     @Id
